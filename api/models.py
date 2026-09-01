12f27811815f3062ca6bccd757ffca6c8811c271
Gyanshu Gupta <gyanshugupta11849@gmail.com>
2026-09-01 17:58:32 +0530

    backend: Forge database models and register to admin

--- api/models.py
+++ api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Station(models.Model):
+    station_id = models.CharField(max_length=50, primary_key=True)
+    name = models.CharField(max_length=100, blank=True, null=True)
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+    sensor_health = models.IntegerField(default=100)
+    status = models.CharField(max_length=20, default='HEALTHY')
+    last_seen = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.station_id
+    
+class SensorReading(models.Model):
+    reading_id = models.CharField(max_length=50, primary_key=True)
+    station = models.ForeignKey(Station, on_delete=models.CASCADE)
+    device_id = models.CharField(max_length=50, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    temperature = models.FloatField()
+    pressure = models.FloatField()
+    humidity = models.FloatField()
+    is_corrected = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.station_id} - {self.timestamp}"
