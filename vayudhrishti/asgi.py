b777b853140bd2d47945a91914e3796829ce5c81
Gyanshu Gupta <gyanshugupta11849@gmail.com>
2026-09-04 20:01:59 +0530

    fix: correct ASGI boot sequence for custom middleware

--- vayudhrishti/asgi.py
+++ vayudhrishti/asgi.py
@@ -1,29 +1,23 @@
-"""
-ASGI config for vayudhrishti project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.1/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-import api.routing
 
-from api.middleware import TokenAuthMiddleware
-
+# 1. SET THE POWER CORE FIRST!
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vayudhrishti.settings')
 
+# 2. INITIALIZE DJANGO (This loads the settings and apps)
+django_asgi_app = get_asgi_application()
+
+# 3. ONLY NOW CAN YOU IMPORT THE ROUTING AND MIDDLEWARE!
+from channels.routing import ProtocolTypeRouter, URLRouter
+import api.routing
+from api.middleware import TokenAuthMiddleware
+
+# 4. ASSEMBLE THE BATTLESTATION
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": TokenAuthMiddleware(
         URLRouter(
             api.routing.websocket_urlpatterns
         )
     ),
-    
 })
