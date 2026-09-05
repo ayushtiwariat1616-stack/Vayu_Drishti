23b40580b6ca371f4f8fb994fe3e8c0f6e744eac
Gyanshu Gupta <gyanshugupta11849@gmail.com>
2026-09-05 13:12:07 +0530

    changed type in consumers.py

--- api/consumers.py
+++ api/consumers.py
@@ -25,6 +25,6 @@ class TelemetryConsumer(AsyncWebsocketConsumer):
     async def send_alert(self, event):
         message = event['message']
         await self.send(text_data=json.dumps({
-            'type': 'new_reading',  # 🛑 THE FIX: Stop screaming anomaly!
+            'type': 'NEW_READING',  # 🛑 THE FIX: Stop screaming anomaly!
             'data': message
         }))
